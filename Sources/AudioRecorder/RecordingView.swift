f70dfd9731273ede3bacbee26d90095a9e45e6d8
sirily11 <32106111+sirily11@users.noreply.github.com>
2024-03-22 00:21:20 +0800

    fix: update recording

--- Sources/AudioRecorder/RecordingView.swift
+++ Sources/AudioRecorder/RecordingView.swift
@@ -14,6 +14,8 @@ import SwiftUI
  */
 public typealias WaveformViewBuilder<T> = (_ data: [Float], _ configuration: AudioManagerConfiguration) -> T
 
+public typealias ButtonViewBuilder<T> = (_ isRecording: Bool) -> T
+
 /**
  Callback function when recorded is finished
  */
@@ -32,7 +34,7 @@ public struct VisulizationConfiguration {
 /**
     A view that allows you to record audio and visualize the waveform
  */
-public struct RecordingView<VisulizationView: View>: View {
+public struct RecordingView<VisulizationView: View, ButtonView: View>: View {
     @StateObject var recorder: AudioManager = .init()
     @State var isRecording = false
     @State var showVisulization = false
@@ -43,12 +45,14 @@ public struct RecordingView<VisulizationView: View>: View {
     let visulizationConfiguration: VisulizationConfiguration
 
     let buildAudioWavefromView: WaveformViewBuilder<VisulizationView>?
+    let buildButtonView: ButtonViewBuilder<ButtonView>?
     let onRecorded: OnRecorded?
 
     public init(recordingConfiguration: AudioManagerConfiguration = .init(), visulizationConfiguration: VisulizationConfiguration = .init(), onRecorded: OnRecorded? = nil, buildAudioWavefromView:
-        @escaping WaveformViewBuilder<VisulizationView>)
+        @escaping WaveformViewBuilder<VisulizationView>, buildButtonView: @escaping ButtonViewBuilder<ButtonView>)
     {
         self.buildAudioWavefromView = buildAudioWavefromView
+        self.buildButtonView = buildButtonView
         self.recordingConfiguration = recordingConfiguration
         self.visulizationConfiguration = visulizationConfiguration
         self.timer = Timer.publish(
@@ -61,6 +65,7 @@ public struct RecordingView<VisulizationView: View>: View {
 
     public init(recordingConfiguration: AudioManagerConfiguration = .init(), visulizationConfiguration: VisulizationConfiguration = .init(), onRecorded: OnRecorded? = nil) {
         self.buildAudioWavefromView = nil
+        self.buildButtonView = nil
         self.timer = Timer.publish(
             every: recordingConfiguration.updateInterval,
             on: .main,
@@ -71,6 +76,9 @@ public struct RecordingView<VisulizationView: View>: View {
         self.onRecorded = onRecorded
     }
 
+    /**
+     Default visulization view
+     */
     var defaultVisulizationView: some View {
         Chart(Array(recorder.amplitudes.enumerated()), id: \.0) { index, magnitude in
             BarMark(
@@ -83,12 +91,47 @@ public struct RecordingView<VisulizationView: View>: View {
         .chartYScale(domain: recorder.configuration.waveformLowerLimit ... recorder.configuration.waveformUpperLimit)
         .padding()
         .frame(width: visulizationConfiguration.waveformWidth, height: visulizationConfiguration.waveformHeight)
+        .foregroundStyle(.yellow)
         .background(
-            .black
+            Color.primary
                 .opacity(0.3)
                 .shadow(.inner(radius: 20))
         )
         .cornerRadius(10)
+    }
+
+    func onButtonClick() {
+        if isRecording {
+            recorder.stopRecording()
+        } else {
+            recorder.startRecording()
+        }
+        withAnimation {
+            showVisulization.toggle()
+            isRecording.toggle()
+        }
+    }
+
+    /**
+     Default button view
+     */
+    var defaultButtonView: some View {
+        Button(action: {
+            onButtonClick()
+        }, label: {
+            Group {
+                if isRecording {
+                    Image(systemName: "stop.fill")
+                } else {
+                    Image(systemName: "mic.fill")
+                }
+            }
+            .font(.system(size: 20))
+            .frame(width: 20, height: 20)
+            .padding()
+        })
+        .background(isRecording ? Color.accentColor : nil)
+        .clipShape(.circle)
     }
 
     public var body: some View {
@@ -104,16 +147,13 @@ public struct RecordingView<VisulizationView: View>: View {
                 }
             }
             .opacity(showVisulization ? 1 : 0)
-            Button(isRecording ? "Stop" : "Start") {
-                if isRecording {
-                    recorder.stopRecording()
-                } else {
-                    recorder.startRecording()
-                }
-                withAnimation {
-                    showVisulization.toggle()
-                    isRecording.toggle()
-                }
+            // slide from bottom
+            .transition(.move(edge: .bottom))
+
+            if let buildButtonView = buildButtonView {
+                buildButtonView(isRecording)
+            } else {
+                defaultButtonView
             }
         }
         .onChange(of: isRecording) { oldValue, newValue in
@@ -139,9 +179,25 @@ public struct RecordingView<VisulizationView: View>: View {
     }
 }
 
-public extension RecordingView where VisulizationView == EmptyView {
+public extension RecordingView where VisulizationView == EmptyView, ButtonView == EmptyView {
     init(recordingConfiguration: AudioManagerConfiguration = .init(), visulizationConfiguration: VisulizationConfiguration = .init(), onRecorded: OnRecorded? = nil) {
         self.buildAudioWavefromView = nil
+        self.buildButtonView = nil
+        self.recordingConfiguration = recordingConfiguration
+        self.timer = Timer.publish(
+            every: recordingConfiguration.updateInterval,
+            on: .main,
+            in: .common
+        ).autoconnect()
+        self.visulizationConfiguration = visulizationConfiguration
+        self.onRecorded = onRecorded
+    }
+}
+
+public extension RecordingView where ButtonView == EmptyView {
+    init(recordingConfiguration: AudioManagerConfiguration = .init(), visulizationConfiguration: VisulizationConfiguration = .init(), onRecorded: OnRecorded? = nil) {
+        self.buildAudioWavefromView = nil
+        self.buildButtonView = nil
         self.recordingConfiguration = recordingConfiguration
         self.timer = Timer.publish(
             every: recordingConfiguration.updateInterval,
